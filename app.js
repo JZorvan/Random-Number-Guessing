39574a20fd81cc0a4c904dfcaa499a40d3bd2232
Janelle Zorvan <jzorvan@gmail.com>
2017-03-14 11:21:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 'use strict';
 
 // Variables
-
 const highestNum = 10000;
 let guess;
 let attempts = 0;
 
 // Random Number Generator
-
 const getRandomNum = (highestNum) => {
   return Math.floor( Math.random() * highestNum) + 1;
 }
 
-// Creates the Number to Guess
-const randomNum = getRandomNum(highestNum);
+// Creates the Target Number
+const targetNum = getRandomNum(highestNum);
 
 // Computer makes guesses until correct, logging # of attempts
-while ( guess !== randomNum ) {
+while ( guess !== targetNum ) {
 	guess = getRandomNum(highestNum);
 	attempts++;
 }
 
 // Print results
-document.write(`<p>The random number is: ${randomNum}</p>`);
+document.write(`<p>The random number is: ${targetNum}</p>`);
 document.write(`<p>The computer guessed it after ${attempts} attempts.</p>`);
